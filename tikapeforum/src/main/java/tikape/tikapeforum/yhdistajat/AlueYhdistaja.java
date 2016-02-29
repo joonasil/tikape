af8d87a36f1f44bc8ad2b0e164de0e527e787b28
juhopaak <juho.paakkonen@helsinki.fi>
2016-02-29 16:11:40 +0200

    yhdistäjiä

---
+++ tikapeforum/src/main/java/tikape/tikapeforum/yhdistajat/AlueYhdistaja.java
@@ -0,0 +1,29 @@
+
+package tikape.tikapeforum.yhdistajat;
+
+import java.util.ArrayList;
+import java.util.List;
+import tikape.tikapeforum.database.taulut.Keskustelu;
+import tikape.tikapeforum.database.taulut.Keskustelualue;
+
+public class AlueYhdistaja {
+    
+    private List<Keskustelualue> alueet;
+    private List<Keskustelu> keskustelut;
+    
+    public AlueYhdistaja(List<Keskustelualue> alueet, List<Keskustelu> keskustelut) {
+        this.alueet = alueet;
+        this.keskustelut = keskustelut;
+    }
+    
+    public void yhdista() {
+        for (Keskustelu keskustelu : keskustelut) {
+            for(Keskustelualue alue : alueet) {
+                if (keskustelu.getAlueId() == alue.getId()) {
+                    alue.lisaaKeskustelu(keskustelu);
+                }
+            }
+        }
+    }
+    
+}
---
+++ tikapeforum/src/main/java/tikape/tikapeforum/yhdistajat/KeskusteluYhdistaja.java
@@ -0,0 +1,26 @@
+
+package tikape.tikapeforum.yhdistajat;
+
+import java.util.List;
+import tikape.tikapeforum.database.taulut.Keskustelu;
+import tikape.tikapeforum.database.taulut.Viesti;
+
+public class KeskusteluYhdistaja {
+    private List<Viesti> viestit;
+    private List<Keskustelu> keskustelut;
+    
+    public KeskusteluYhdistaja(List<Keskustelu> keskustelut, List<Viesti> viestit) {
+        this.viestit = viestit;
+        this.keskustelut = keskustelut;
+    }
+    
+    public void yhdista() {
+        for (Viesti viesti : viestit) {
+            for(Keskustelu keskustelu : keskustelut) {
+                if (keskustelu.getId() == viesti.getKeskusteluId()) {
+                    keskustelu.lisaaViesti(viesti);
+                }
+            }
+        }
+    }
+}
